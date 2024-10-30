214541a6693983dacf0c0735f9650ee19e0ac33c
David G.Muñoz <72562392+davidgregoriom@users.noreply.github.com>
2024-10-30 13:48:25 +0100

    Actualización del Main para evitar errores en deploy

--- main.ts
+++ main.ts
@@ -7,10 +7,12 @@ const MONGO_URL = Deno.env.get("MONGO_URL");
 
 if (!MONGO_URL) {
   console.log("No mongo URL found");
-  Deno.exit(1);
+  //Deno.exit(1);
+}else{
+  await mongoose.connect(MONGO_URL);
 }
 
-await mongoose.connect(MONGO_URL);
+
 const app = express();
 app.use(cors());
 app.use(express.json());
